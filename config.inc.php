e7f32ed0c19ac01bf420e953822061e479ee5048
hivefans <shidongjie@gmail.com>
2013-05-27 10:10:51 +0800

    comit code

---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$configure = array();
+
+//$configure['root_path'] = __DIR__;
+//$configure['hbase_host'] = '192.168.205.6';
+$configure['hbase_host'] ='114.112.82.20';
+$configure['hbase_port'] = '9090';
+$configure['language'] = 'english';
+?>
