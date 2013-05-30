32d12251a639b29e0b11a6660f9ab44beeb68142
hivefans <shidongjie@gmail.com>
2013-05-30 19:22:44 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -3,8 +3,8 @@
 $configure = array();
 
 //$configure['root_path'] = __DIR__;
-//$configure['hbase_host'] = '192.168.205.6';
-$configure['hbase_host'] ='114.112.82.20';
+$configure['hbase_host'] = '192.168.205.6';
+
 $configure['hbase_port'] = '9090';
 $configure['language'] = 'english';
 ?>
